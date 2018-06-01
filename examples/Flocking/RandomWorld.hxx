c0c8406f1f758d99300b8cae612333fe75b9b983
Quim <quim.lazaro@bsc.es>
2018-06-01 16:31:29 +0200

    Docu Flocking

--- examples/Flocking/RandomWorld.hxx
+++ examples/Flocking/RandomWorld.hxx
@@ -11,10 +11,14 @@ class RandomWorldConfig;
 
 class RandomWorld : public Engine::World
 {
+	//crea i inicialitza els Rasters sobre els que es fara la simulacio
 	void createRasters();
+	//crea i inicialitza els agents que duuran a terme la simulacio
 	void createAgents();
 public:
+	//crea el World i inicialitza el scheduler
 	RandomWorld(Engine::Config * config, Engine::Scheduler * scheduler = 0);
+	//destructora per defecte
 	virtual ~RandomWorld();
 };
 
