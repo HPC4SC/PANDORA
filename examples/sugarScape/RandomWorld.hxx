fc0b2406e389ff1a29df4227c82b50cd890b1779
Quim <quim.lazaro@bsc.es>
2018-07-03 12:31:29 +0200

    ara si sugarScape ben fet

--- examples/sugarScape/RandomWorld.hxx
+++ examples/sugarScape/RandomWorld.hxx
@@ -3,6 +3,7 @@
 #define __RandomWorld_hxx
 
 #include <World.hxx>
+#include <vector>
 
 namespace Examples 
 {
@@ -11,10 +12,17 @@ class RandomWorldConfig;
 
 class RandomWorld : public Engine::World
 {
+	typedef std::vector<char> Row;
+	typedef std::vector<Row> Matrix;
+	
+	Matrix gridValues{50,Row(50)}; // attribute that represents the max values of each cell
+	
 	// creates and initializes the Rasters of the simulation
 	void createRasters();
 	// creates and initializes the Agents of the simulation
 	void createAgents();
+	// initializes the Matrix with the given values
+	void initValues();
 public:
 	// creates a RandomWold instance
 	RandomWorld(Engine::Config * config, Engine::Scheduler * scheduler = 0);
