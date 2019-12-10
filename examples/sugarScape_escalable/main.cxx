63c9912d3710ffef915fc2a594e0d651d8aa9543
QuimLaz <quimlaz@gmail.com>
2019-12-10 12:21:52 +0100

    first verion IoTwins, map loading tests

--- examples/sugarScape_escalable/main.cxx
+++ examples/sugarScape_escalable/main.cxx
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		{
 			fileName = argv[1];
 		}
-		Examples::RandomWorld world(new Examples::RandomWorldConfig(fileName), world.useSpacePartition(2,false));
+		Examples::RandomWorld world(new Examples::RandomWorldConfig(fileName), world.useSpacePartition(2));
 	
 		world.initialize(argc, argv);
 		world.run();
