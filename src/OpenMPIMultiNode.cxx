f4e798de2ef0b8fb43768219a6efe3947726f11e
German <ge.najim@gmail.com>
2020-06-12 19:25:48 +0200

    Code without testing and not compiled.

--- src/OpenMPIMultiNode.cxx
+++ src/OpenMPIMultiNode.cxx
@@ -23,6 +23,8 @@
 
 namespace Engine {
 
+    // PUBLIC METHODS //
+
     OpenMPIMultiNode::OpenMPIMultiNode() 
     {
         _root = NULL;
@@ -49,12 +51,14 @@ namespace Engine {
         // _boundaries._origin = Point2D<int>( 0, 0 );
         // _boundaries._size = _world->getConfig( ).getSize( );
         _boundaries = Rectangle<int>(_world->getConfig().getSize());
+
+        initTree();
     }
 
     void OpenMPIMultiNode::divideSpace()
     {
-        //for (AgentsList::iterator it = _world->beginAgents(); it != _world->endAgents(); ++it) 
-        // ITERATE OVER THE _boundaries SPACE, LOOKING FOR AGENTS
+        _totalAgentsWeight = (double) _world->getNumberOfAgents();   // This can be changed so a 'weight' for an agent can be anything. Here all agents weight the same: 1
+        divideSpaceRecursive(_root, _totalAgentsWeight, (int) std::log2(_numberOfMPITasks) + 1);
     }
 
     void OpenMPIMultiNode::sendSpaces()
@@ -62,9 +66,43 @@ namespace Engine {
 
     }
 
-    node* OpenMPIMultiNode::insertNode()
-    {
+    // PROTECTED METHODS //
 
+    void OpenMPIMultiNode::initTree() 
+    {
+        _root = new node<Rectangle<int>>;
+        _root->value = Rectangle<int>(_world->getConfig().getSize());
+        _root->left = NULL;
+        _root->right = NULL;
+    }
+
+    int OpenMPIMultiNode::numberOfLeafs(node<Rectangle<int>>* node)
+    {
+        if (node == NULL) return 0;
+        if (node->left == NULL and node->right == NULL) return 1;
+        return numberOfLeafs(node->left) + numberOfLeafs(node->right);
+    }
+
+    node<Rectangle<int>>* OpenMPIMultiNode::insertNode(const Rectangle<int>& rectangle, node<Rectangle<int>>* treeNode)
+    {
+        if (treeNode->left == NULL)
+        {
+            treeNode->left = new node<Rectangle<int>>;
+            treeNode->left->value = rectangle;
+            treeNode->left->left = NULL;
+            treeNode->left->right = NULL;
+
+            return treeNode->left;
+        }
+        else if (treeNode->right == NULL)
+        {
+            treeNode->right = new node<Rectangle<int>>;
+            treeNode->right->value = rectangle;
+            treeNode->right->left = NULL;
+            treeNode->right->right = NULL;
+
+            return treeNode->right;
+        }
     }
 
     void OpenMPIMultiNode::destroyTree(node* leaf)
@@ -74,6 +112,96 @@ namespace Engine {
             destroyTree(leaf->left);
             destroyTree(leaf->right);
             delete leaf;
+        }
+    }
+
+    double OpenMPIMultiNode::getAgentWeight(const Agent& agent)
+    {
+        // Make a map<agentsId, agentsWeight> for more complex implementations
+        return 1;  // Simplest implementation
+    }
+
+    double OpenMPIMultiNode::getAgentsWeight(const AgentsVector& agentsVector)
+    {
+        double totalWeight = 0;
+        for (i = 0; i < agentsVector.size(); ++i) totalWeight += getAgentWeight(agentsVector[i].get());
+        return totalWeight;
+    }
+
+    AgentsVector OpenMPIMultiNode::getAgentsInPosition(const Point2D<int>& position, const std::string& type)
+    {
+        AgentsVector result;
+        for (AgentsList::iterator it = _world->beginAgents(); it != _world->endAgents(); ++it)
+        {
+            AgentPtr agent = *it;
+            if (agent->getPosition().isEqual(position))
+            {
+                if (type.compare("all") == 0 or agent->isType(type)) result.push_back(agent);
+            }
+        }
+        return result;
+    }
+
+    double OpenMPIMultiNode::getAgentsWeightFromCell(const int& row, const int& column)
+    {
+        Point2D<int> position(row, column);
+        AgentsVector agentsVector = getAgentsInPosition(position);
+        return getAgentsWeight(agentsVector);
+    }
+
+    void OpenMPIMultiNode::divideSpaceRecursive(node<Rectangle<int>>* treeNode, const double& totalWeight, const int& currentDepth)
+    {
+        if (currentDepth == 0 or numberOfLeafs(_root) == _numberOfMPITasks) return 0;
+
+        double leftChildTotalWeight = 0;
+        bool stopExploration = false;
+
+        if (currentDepth % 2 == 0) {   // Horizontal exploration
+            for (int i = treeNode->value.left(); i <= treeNode->value.right() and not stopExploration; ++i)
+            {
+                for (int j = treeNode->value.top(); j <= treeNode->value.bottom(); ++j) 
+                    leftChildTotalWeight += getAgentsWeightFromCell(j, i);
+
+                if (leftChildTotalWeight >= totalWeight / 2)
+                {
+                    stopExploration = true;
+
+                    Rectangle<int> leftRectangle(treeNode->value.left(), treeNode->value.top(), i, treeNode->value.bottom());
+                    Rectangle<int> rightRectangle(i + 1, treeNode->value.top(), treeNode->value.right(), treeNode->value.bottom());
+                    node<Rectangle<int>>* leftNode = insertNode(leftRectangle, treeNode);
+                    node<Rectangle<int>>* rightNode = insertNode(rightRectangle, treeNode);
+
+                    divideSpaceRecursive(leftNode, leftChildTotalWeight, depth - 1);
+                    divideSpaceRecursive(rightNode, totalWeight - leftChildTotalWeight, depth - 1);
+                }
+
+                leftChildTotalWeight = 0;
+            }
+        }
+        else {                  // Vertical exploration
+            for (int i = _boundaries.top(); i < _boundaries.bottom(); ++i) {
+                for (int j = _boundaries.left(); j < _boundaries.right(); ++j) {
+                    // to be implemented
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+            }
         }
     }
 
