d948b8a0e85507599c58a28875cdc6abfcf19d2a
Quim <quimlaz@gmail.com>
2020-08-10 15:48:09 +0200

    contact_tracing structure

---
+++ examples/contact_tracing/SupermarketConfig.cxx
@@ -0,0 +1,35 @@
+#include <SupermarketConfig.hxx>
+
+namespace Examples 
+{
+
+SupermarketConfig::SupermarketConfig(const std::string & xmlFile) : Config(xmlFile) {}
+
+SupermarketConfig::~SupermarketConfig() {}
+
+void SupermarketConfig::loadParams() {
+    _clientRate = getParamInt("Client","clientRate");
+    _drifting = getParamFloat("Client","drifting");
+    _stopping = getParamFloat("Client","stopping");
+    _stopTime = getParamInt("Client","stopTime");
+    _wander = getParamFloat("Client","wander");
+
+    _numCashiers = getParamInt("Cashier","numCashiers");
+    _sickCashiers = getParamInt("Cashier", "sickCashiers");
+    _cashierShift = getParamInt("Cashier","cashierShift");
+
+    _mapRoute = getParamStr("Raster","mapRoute");
+
+    _infectiosness = getParamFloat("Epidemiology","infectiousness");
+    _infectoinRadius = getParamInt("Epidemiology","infectoinRadius");
+    _sickRate = getParamFloat("Epidemiology","sickRate");
+    _encounterRadius = getParamInt("Epidemiology","encounterRadius");
+    _maxSick = getParamInt("Epidemiology","maxSick");
+
+    _signalRadius = getParamInt("Phone","signalRadius");
+    _applicationRate = getParamFloat("Phone","applicationRate");
+    _phoneThreshold1 = getParamInt("Phone","phoneThreshold1");
+    _phoneThreshold2 = getParamInt("Phone","phoneThreshold2");
+}
+
+}
