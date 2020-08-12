257a19ac705eb57a3ec61bdca52c2111f061ffbc
Quim <quimlaz@gmail.com>
2020-08-12 17:01:16 +0200

    bugfix with counting encounters

--- examples/contact_tracing/HumanBeeing.cxx
+++ examples/contact_tracing/HumanBeeing.cxx
@@ -30,6 +30,12 @@ Phone* HumanBeeing::getPhonePointer() {
 void HumanBeeing::updateKnowledge() {
     countEncountersReal();
     countEncountersRecorded();
+    std::cout << "Encounters real: " << getId() << " ";
+    for (int i = 0; i < _encountersReal.size(); i++) std::cout << _encountersReal[i].first << "," << _encountersReal[i].second << " ";
+    std::cout << std::endl;
+    //std::cout << "Encounters recorded: ";
+    //for (int i = 0; i < _encountersRecorded.size(); i++) std::cout << _encountersRecorded[i].first << "," << _encountersRecorded[i].second << " ";
+    //std::cout << std::endl;
 }
 
 void HumanBeeing::createPhone(const int& threshold1, const int& threshold2, const bool& hasApplication, const int& signalRaius) {
@@ -40,11 +46,18 @@ void HumanBeeing::countEncountersReal() {
     Engine::AgentsVector neighbours = _world->getNeighbours(this, _encounterRadius);
     for (int i = 0; i < neighbours.size(); i++) {
         if (neighbours[i]->getId() != getId()) {
-            for (int j = 0; j < _encountersReal.size(); j++) {
-                if (_encountersReal[j].first == neighbours[i]->getId()) _encountersReal[j].second += 1;
-                else _encountersReal.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
-                _timeSpentWithOthers += 1;
+            if (_encountersReal.size() == 0) _encountersReal.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
+            else {
+                bool in = false;
+                for (int j = 0; j < _encountersReal.size(); j++) {
+                    if (_encountersReal[j].first == neighbours[i]->getId()) {
+                        in = true;
+                        _encountersReal[j].second += 1;
+                    }
+                }
+                if (not in) _encountersReal.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
             }
+            _timeSpentWithOthers += 1;
         }
     }
 }
@@ -56,12 +69,15 @@ void HumanBeeing::countEncountersRecorded() {
         Engine::AgentsVector::iterator neighbour = neighbours.begin();
         for (int i = 0; i < neighbours.size(); i++) {
             if (neighbours[i]->getId() != getId()) {
-                for (int j = 0; j < _encountersRecorded.size(); j++) {
-                    if (_encountersRecorded[j].first == neighbours[i]->getId()) _encountersRecorded[j].second += 1;
-                    else _encountersRecorded.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
-                    HumanBeeing* person = dynamic_cast<HumanBeeing*>(neighbour->get());
-                    int detection = person->phoneListen(_sick,getPosition().distance(person->getPosition()));
-                    counted += detection;
+                if (_encountersRecorded.size() == 0) _encountersRecorded.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
+                else {
+                    for (int j = 0; j < _encountersRecorded.size(); j++) {
+                        if (_encountersRecorded[j].first == neighbours[i]->getId()) _encountersRecorded[j].second += 1;
+                        else _encountersRecorded.push_back(std::pair<std::string,int>(neighbours[i]->getId(),1));
+                        HumanBeeing* person = dynamic_cast<HumanBeeing*>(neighbour->get());
+                        int detection = person->phoneListen(_sick,getPosition().distance(person->getPosition()));
+                        counted += detection;
+                    }
                 }
             }
             neighbour++;
