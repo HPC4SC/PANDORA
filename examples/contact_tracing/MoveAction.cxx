60543df3e33eaeeaa0c5599d83d029fc89b44a24
Quim <quimlaz@gmail.com>
2020-08-13 09:52:34 +0200

    bugfix with counting

--- examples/contact_tracing/MoveAction.cxx
+++ examples/contact_tracing/MoveAction.cxx
@@ -11,7 +11,7 @@ MoveAction::MoveAction() {}
 MoveAction::~MoveAction() {}
 
 void MoveAction::execute(Engine::Agent & agent) { // TODO fer servir A*
-	std::cout << "Move Agents by: " << agent.getId() << std::endl;
+	//std::cout << "Move Agents by: " << agent.getId() << std::endl;
     Engine::World* world = agent.getWorld();
 	
 	Engine::Point2D<int> newPosition = agent.getPosition();
