c921d263c33ef4680cd5322c252af4d4d191950e
Quim <quimlaz@gmail.com>
2020-09-01 12:33:47 +0200

    rm prints and fix warnings

--- examples/contact_tracing/Phone.cxx
+++ examples/contact_tracing/Phone.cxx
@@ -10,7 +10,7 @@ namespace Examples
 {
 
 Phone::Phone(const int& threshold1, const int& threshold2, const bool& hasApplicattion, const int& signalRadius) 
-: _threshold1(_threshold1), _threshold2(threshold2), _hasApplication(hasApplicattion), _signalRadius(signalRadius), _variance(5),
+: _threshold1(threshold1), _threshold2(threshold2), _hasApplication(hasApplicattion), _signalRadius(signalRadius), _variance(5),
     _maxDistance(100) {
     _startOffset = Engine::GeneralState::statistics().getUniformDistValue(0,300);
     _buckets = std::vector<int>(3);
