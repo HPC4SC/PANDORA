abb8d6f7760cf39facaf3e5de4196ac820995aa3
Quim <quimlaz@gmail.com>
2020-09-14 12:16:57 +0200

    change visualization script for all scenarios

--- examples/contact_tracing/results/visualizationSuper.py
+++ examples/contact_tracing/results/visualizationSuper.py
@@ -7,15 +7,22 @@ import seaborn as sns
 from celluloid import Camera
 import csv
 
-def read_agents(agent_route):
+def read_agents(agent_route,scenario):
     with h5py.File(agent_route, 'r') as hdf:
         #base_items = list(hdf.items())
         #print('Items in the base directory:', base_items)
-        simulation = hdf.get('Client')
+        if scenario == '0':
+            agentType = 'Client'
+        elif scenario == '1':
+            agentType = 'Walker'
+        elif scenario == '2':
+            agentType = 'Passenger'
+
+        simulation = hdf.get(agentType)
         #print(len(simulation))
         steps_dfs = []
         for i in range(0,len(simulation)):
-            G1 = hdf.get('/Client/step' + str(i))
+            G1 = hdf.get('/' + agentType + '/step' + str(i))
             G1_items = list(G1.items())
             #print(G1_items)
             SICKcombinedBucketsNotifications = np.array(G1_items[0][1])
@@ -30,34 +37,35 @@ def read_agents(agent_route):
             timeInSimulation = np.array(G1_items[9][1])
             x_coords = np.array(G1_items[10][1])
             y_coords = np.array(G1_items[11][1])
-            G2 = hdf.get('/Cashier/step' + str(i))
-            G2_items = list(G2.items())
-            SICKcombinedBucketsNotifications_cashier = np.array(G2_items[0][1])
-            SICKfirstBucketNotifications_cashier = np.array(G2_items[1][1])
-            combinedBucketsNotifications_cashier = np.array(G2_items[2][1])
-            countInfected_cashier = np.array(G2_items[3][1])
-            firstBucketNotifications_cashier = np.array(G2_items[4][1])
-            ids_cashiers = np.array(G2_items[5][1])
-            infected_cashier = np.array(G2_items[6][1])
-            infectionTime_cashier = np.array(G2_items[7][1])
-            sick_cashier = np.array(G2_items[8][1])
-            timeInSimulation_cashier = np.array(G2_items[9][1])
-            x_cashiers = np.array(G2_items[10][1])
-            y_cashiers = np.array(G2_items[11][1])
+            if scenario == '0':    
+                G2 = hdf.get('/Cashier/step' + str(i))
+                G2_items = list(G2.items())
+                SICKcombinedBucketsNotifications_cashier = np.array(G2_items[0][1])
+                SICKfirstBucketNotifications_cashier = np.array(G2_items[1][1])
+                combinedBucketsNotifications_cashier = np.array(G2_items[2][1])
+                countInfected_cashier = np.array(G2_items[3][1])
+                firstBucketNotifications_cashier = np.array(G2_items[4][1])
+                ids_cashiers = np.array(G2_items[5][1])
+                infected_cashier = np.array(G2_items[6][1])
+                infectionTime_cashier = np.array(G2_items[7][1])
+                sick_cashier = np.array(G2_items[8][1])
+                timeInSimulation_cashier = np.array(G2_items[9][1])
+                x_cashiers = np.array(G2_items[10][1])
+                y_cashiers = np.array(G2_items[11][1])
 
-            if ids.size != 0 and ids_cashiers.size != 0:
-                SICKcombinedBucketsNotifications = np.append(SICKcombinedBucketsNotifications,SICKcombinedBucketsNotifications_cashier)
-                SICKfirstBucketNotifications = np.append(SICKfirstBucketNotifications,SICKfirstBucketNotifications_cashier)
-                combinedBucketsNotifications = np.append(combinedBucketsNotifications,combinedBucketsNotifications_cashier)
-                countInfected = np.append(countInfected,countInfected_cashier)
-                firstBucketNotifications = np.append(firstBucketNotifications,firstBucketNotifications_cashier)
-                ids = np.append(ids,ids_cashiers)
-                infected = np.append(infected,infected_cashier)
-                infectionTime = np.append(infectionTime,infectionTime_cashier)
-                sick = np.append(sick,sick_cashier)
-                timeInSimulation = np.append(timeInSimulation,timeInSimulation_cashier)
-                x_coords = np.append(x_coords,x_cashiers)
-                y_coords = np.append(y_coords,y_cashiers)
+                if ids.size != 0 and ids_cashiers.size != 0:
+                    SICKcombinedBucketsNotifications = np.append(SICKcombinedBucketsNotifications,SICKcombinedBucketsNotifications_cashier)
+                    SICKfirstBucketNotifications = np.append(SICKfirstBucketNotifications,SICKfirstBucketNotifications_cashier)
+                    combinedBucketsNotifications = np.append(combinedBucketsNotifications,combinedBucketsNotifications_cashier)
+                    countInfected = np.append(countInfected,countInfected_cashier)
+                    firstBucketNotifications = np.append(firstBucketNotifications,firstBucketNotifications_cashier)
+                    ids = np.append(ids,ids_cashiers)
+                    infected = np.append(infected,infected_cashier)
+                    infectionTime = np.append(infectionTime,infectionTime_cashier)
+                    sick = np.append(sick,sick_cashier)
+                    timeInSimulation = np.append(timeInSimulation,timeInSimulation_cashier)
+                    x_coords = np.append(x_coords,x_cashiers)
+                    y_coords = np.append(y_coords,y_cashiers)
             
             data = {
             'SICKcombinedBucketsNotifications': SICKcombinedBucketsNotifications,
@@ -95,8 +103,8 @@ def generateOutput(steps_dfs,concat):
     infected = len(agent_records[agent_records['infected']>0])
 
     return {
-        'meanTimeInStore': agent_records['timeInSimulation'].mean(),
-        'medianTimeInStore': agent_records['timeInSimulation'].median(),
+        'meanTimeInSimulation': agent_records['timeInSimulation'].mean(),
+        'medianTimeInSimulation': agent_records['timeInSimulation'].median(),
         'avgPeoplePerMinute': total_people/(steps/60),
         'totalPeople': total_people,
         'previouslySick': sum(agent_records['sick']),
@@ -112,13 +120,36 @@ def generateOutput(steps_dfs,concat):
         'falsePositives': sum(agent_records['falsePositives'])/total_people 
     }
 
-def print_agents(steps_dfs):
+def print_agents(steps_dfs,scenario):
+    if scenario == '0':
+        x1 = 0
+        y1 = 40
+        x2 = 40
+        y2 = 0
+    elif scenario == '1':
+        x1 = 0
+        y1 = 100
+        x2 = 10
+        y2 = 0
+    elif scenario == '2':
+        x1 = 0
+        y1 = 258
+        x2 = 9
+        y2 = 0
     concat = pd.concat(steps_dfs)
     result = []
     result.append(generateOutput(steps_dfs,concat))
-    pd.DataFrame(result).to_csv('./output.csv')
+    if scenario == '0':
+        type = 'Supermarket'
+        img = plt.imread("../maps/spar2.png")
+    elif scenario == '1':
+        type = 'Street'
+        img = plt.imread("../maps/street_layout.png")
+    elif scenario == '2':
+        type = 'Train' 
+        img = plt.imread("../maps/train_layout.png")
+    pd.DataFrame(result).to_csv('./' + type + 'Output.csv')
 
-    img = plt.imread("../maps/spar2.png")
     fig, ax = plt.subplots()
 
     camera = Camera(fig)
@@ -132,19 +163,19 @@ def print_agents(steps_dfs):
         points.append((x,y))
     for i in range(len(points)):
         ax.imshow(img)
-        plt.xlim(0,40)
-        plt.ylim(40,0)
-        plt.scatter(x[i],y[i],s=4,color='white',marker='o')
+        plt.xlim(x1,x2)
+        plt.ylim(y1,y2)
+        plt.scatter(x[i],y[i],s=4,color='grey',marker='o')
         camera.snap()
     anim = camera.animate(blit=True)
     anim.save('animation.htm')
 
-def view_agents(espai_agents):
-    steps_dfs = read_agents(espai_agents)
-    print_agents(steps_dfs)
+def view_agents(espai_agents,scenario):
+    steps_dfs = read_agents(espai_agents,scenario)
+    print_agents(steps_dfs,scenario)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('You need to specify the route of the agent data file')
     else:
-        view_agents(sys.argv[1])
+        view_agents(sys.argv[1],sys.argv[2])
